71020b9d887f888456814e311f937a21000eb36d
RA <ra_lebedev@mail.ru>
2020-02-03 11:45:35 +0600

    Создана таблица apple

--- console/migrations/m200202_172835_addTableApple.php
+++ console/migrations/m200202_172835_addTableApple.php
@@ -14,6 +14,10 @@ class m200202_172835_addTableApple extends Migration
     {
         $this->createTable("apple",[
             'id' => $this->primaryKey(),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+            'status' => $this->integer(),
+            'how_much_eat' => $this->decimal(3, 2),
         ]);
 
     }
@@ -23,9 +27,7 @@ class m200202_172835_addTableApple extends Migration
      */
     public function safeDown()
     {
-        echo "m200202_172835_addTableApple cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('apple');
     }
 
     /*
